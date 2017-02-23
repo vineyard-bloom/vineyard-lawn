857ab0bac9020e308dc293a1d74ddbf8cd340ec4
cj <christopherwjohnson@gmail.com>
2017-02-23 11:20:09 -0700

    version 0.1.1.  Added js files.

---
+++ source/errors.js
@@ -0,0 +1,46 @@
+// Lawn will handle any type of thrown errors, but also provides these helper Error types.
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var HTTP_Error = (function (_super) {
+    __extends(HTTP_Error, _super);
+    function HTTP_Error(message, status) {
+        if (message === void 0) { message = "Server Error"; }
+        if (status === void 0) { status = 500; }
+        _super.call(this, message);
+        this.status = status;
+    }
+    return HTTP_Error;
+}(Error));
+exports.HTTP_Error = HTTP_Error;
+var Bad_Request = (function (_super) {
+    __extends(Bad_Request, _super);
+    function Bad_Request(message) {
+        if (message === void 0) { message = "Bad request"; }
+        _super.call(this, message, 400);
+    }
+    return Bad_Request;
+}(HTTP_Error));
+exports.Bad_Request = Bad_Request;
+var Needs_Login = (function (_super) {
+    __extends(Needs_Login, _super);
+    function Needs_Login(message) {
+        if (message === void 0) { message = "This request requires a logged in user."; }
+        _super.call(this, message, 401);
+    }
+    return Needs_Login;
+}(HTTP_Error));
+exports.Needs_Login = Needs_Login;
+var Unauthorized = (function (_super) {
+    __extends(Unauthorized, _super);
+    function Unauthorized(message) {
+        if (message === void 0) { message = "You are not authorized to perform this request."; }
+        _super.call(this, message, 403);
+    }
+    return Unauthorized;
+}(HTTP_Error));
+exports.Unauthorized = Unauthorized;
+//# sourceMappingURL=errors.js.map
---
+++ source/lawn.js
@@ -0,0 +1,73 @@
+// Vineyard Lawn
+// version 0.1.0
+// created by Christopher W. Johnson
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+var body_parser = require('body-parser');
+__export(require('./errors'));
+// const json_parser = body_parser.json()
+var json_temp = body_parser.json();
+var json_parser = function (req, res, next) {
+    json_temp(req, res, next);
+};
+(function (Method) {
+    Method[Method["get"] = 0] = "get";
+    Method[Method["post"] = 1] = "post";
+})(exports.Method || (exports.Method = {}));
+var Method = exports.Method;
+function handle_error(res, error) {
+    var status = error.status || 500;
+    console.error("Error", status, error.message);
+    var message = status == 500 ? "Server Error" : error.message;
+    res.status(status).send({
+        message: message
+    });
+}
+exports.handle_error = handle_error;
+// This function is currently modifying req.body for performance though could be changed if it ever caused problems.
+function get_arguments(req) {
+    var result = req.body || {};
+    for (var i in req.query) {
+        result[i] = req[i];
+    }
+    return result;
+}
+function create_handler(endpoint) {
+    return function (req, res) {
+        try {
+            endpoint.action(get_arguments(req))
+                .then(function (content) {
+                res.send(content);
+            }, function (error) {
+                handle_error(res, error);
+            });
+        }
+        catch (error) {
+            handle_error(res, error);
+        }
+    };
+}
+exports.create_handler = create_handler;
+function attach_handler(app, method, route, handler) {
+    if (route[0] != '/')
+        route = '/' + route;
+    if (method == Method.get) {
+        app.get(route, handler);
+    }
+    else {
+        app.post(route, json_parser, handler);
+    }
+}
+exports.attach_handler = attach_handler;
+// initialize_endpoints() is the primary entry point
+function initialize_endpoints(app, endpoints) {
+    for (var _i = 0, endpoints_1 = endpoints; _i < endpoints_1.length; _i++) {
+        var endpoint = endpoints_1[_i];
+        var handler = create_handler(endpoint);
+        attach_handler(app, endpoint.method, endpoint.path, handler);
+    }
+}
+exports.initialize_endpoints = initialize_endpoints;
+//# sourceMappingURL=lawn.js.map
