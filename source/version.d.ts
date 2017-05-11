b5aa27749b30a31d4f0e619dd9c8e50de8b17168
cj <christopherwjohnson@gmail.com>
2017-05-11 11:19:36 -0600

    Line ending tweaks.

---
+++ source/version.d.ts
@@ -0,0 +1,8 @@
+export declare class Version {
+    major: number;
+    minor: number;
+    platform: string;
+    private createFromString(text);
+    constructor(majorOrString: number | string, minor?: number, platform?: string);
+    equals(version: Version): boolean;
+}
---
+++ source/version.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var errors_1 = require("./errors");
+var pattern = /(\d+)\.(\d+)(\.[a-z]+)?/;
+var defaultPlatform = 'none';
+var Version = (function () {
+    function Version(majorOrString, minor, platform) {
+        if (minor === void 0) { minor = 0; }
+        if (platform === void 0) { platform = defaultPlatform; }
+        if (typeof majorOrString === 'string') {
+            this.createFromString(majorOrString);
+        }
+        else {
+            this.major = majorOrString;
+            this.minor = minor;
+            this.platform = platform;
+        }
+    }
+    Version.prototype.createFromString = function (text) {
+        var match = text.match(pattern);
+        if (!match)
+            throw new errors_1.Bad_Request('Invalid version format.');
+        this.major = parseInt(match[1]);
+        this.minor = parseInt(match[2]);
+        this.platform = match[3]
+            ? match[3]
+            : defaultPlatform;
+    };
+    Version.prototype.equals = function (version) {
+        return this.major == version.major && this.minor == version.minor;
+    };
+    return Version;
+}());
+exports.Version = Version;
+//# sourceMappingURL=version.js.map
