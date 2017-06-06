d76f5478af0896beca1bc3fd6f3b5f6f916198cb
cj <christopherwjohnson@gmail.com>
2017-06-06 16:23:51 -0600

    Was missing generated types files.

---
+++ source/types.d.ts
@@ -0,0 +1,25 @@
+export declare type PromiseOrVoid = Promise<any> | null;
+import { Version } from "./version";
+export declare enum Method {
+    get = 0,
+    post = 1,
+    put = 2,
+    delete = 3,
+}
+export interface Request {
+    data: any;
+    session: any;
+    user?: any;
+    params?: any;
+    version: Version;
+    startTime?: any;
+}
+export interface SimpleResponse {
+    code: number;
+    message: string;
+    body: any;
+}
+export interface RequestListener {
+    onRequest(request: Request, response: SimpleResponse, req: any): PromiseOrVoid;
+    onError(error: any, request?: Request): PromiseOrVoid;
+}
---
+++ source/types.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Method;
+(function (Method) {
+    Method[Method["get"] = 0] = "get";
+    Method[Method["post"] = 1] = "post";
+    Method[Method["put"] = 2] = "put";
+    Method[Method["delete"] = 3] = "delete";
+})(Method = exports.Method || (exports.Method = {}));
+//# sourceMappingURL=types.js.map
